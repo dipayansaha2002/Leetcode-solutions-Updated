ce9da915d66d8596af310f0b3e5551376ed8a5f0
dipayansaha2002 <121316056+dipayansaha2002@users.noreply.github.com>
2023-12-10 14:47:07 +0530

    Time: 4 ms (66.67%), Space: 17.6 MB (50.00%) - LeetHub

--- 2960-count-tested-devices-after-test-operations/2960-count-tested-devices-after-test-operations.cpp
+++ 2960-count-tested-devices-after-test-operations/2960-count-tested-devices-after-test-operations.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int countTestedDevices(vector<int>& batteryPercentages) {
         int tested_devices = 0;
-        for(int i =0 ; i < batteryPercentages.size(); ++i )
-        {
-            if(batteryPercentages[i]-tested_devices > 0)
+        for(int i = 0; i < batteryPercentages.size(); ++i) {
+            if(batteryPercentages[i] > tested_devices) { 
                 tested_devices++;
+            }
         }
         return tested_devices;
     }
