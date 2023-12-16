500c2668d29810cb939e6975f612aa96fc869488
dipayansaha2002 <121316056+dipayansaha2002@users.noreply.github.com>
2023-12-17 00:07:30 +0530

    Time: 179 ms (10.92%), Space: 124.2 MB (64.47%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,27 +1,16 @@
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> ans(n);
+        vector<int> ans(nums.size(), 0);
+        int pos = 0, neg = 1;
 
-        int posIdx = 0, negIdx = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) {
-                // Place positive numbers at even indices
-                while (posIdx < n && nums[posIdx] <= 0) {
-                    posIdx++;
-                }
-                ans[i] = nums[posIdx++];
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] < 0) {
+                ans[neg] = nums[i];
+                neg += 2;
             } else {
-                // Place negative numbers at odd indices
-                while (negIdx < n && nums[negIdx] >= 0) {
-                    negIdx++;
-                }
-                ans[i] = nums[negIdx++];
+                ans[pos] = nums[i];
+                pos += 2;
             }
         }
 
