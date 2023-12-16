3546a0f63238fe366e6b5c86109cf007326b908a
dipayansaha2002 <121316056+dipayansaha2002@users.noreply.github.com>
2023-12-16 23:32:24 +0530

    Time: 15 ms (68.07%), Space: 21.7 MB (70.31%) - LeetHub

--- 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.cpp
+++ 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.cpp
@@ -3,10 +3,10 @@ public:
     vector<int> sortArrayByParityII(vector<int>& nums) {
         int n = nums.size();
         int i = 0 ;//i stands for even
-        int j = n-1 ;//j stands for odd
-        while(i < n && j >= 0) {
+        int j = 1 ;//j stands for odd
+        while(i < n && j < n) {
             if   (nums[i]%2 == 0)    i += 2;
-            else if(nums[j] %2 == 1) j -= 2;
+            else if(nums[j] %2 == 1) j += 2;
             else        
                 swap(nums[i], nums[j]);
         }
