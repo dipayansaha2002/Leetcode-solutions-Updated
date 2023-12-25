4c280ea59e88668df93881edd3e5063f094b21b1
dipayansaha2002 <121316056+dipayansaha2002@users.noreply.github.com>
2023-12-25 14:47:59 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (44.81%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        if (s.empty() || s[0] == '0') {
+            return 0;
+        }
+        int n = s.length();
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; ++i) {
+            int oneDigit = s[i - 1] - '0';
+            int twoDigits = stoi(s.substr(i - 2, 2));
+
+            if (oneDigit != 0) {
+                dp[i] += dp[i - 1];
+            }
+            if (10 <= twoDigits && twoDigits <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+        return dp[n];
+    }
+};
