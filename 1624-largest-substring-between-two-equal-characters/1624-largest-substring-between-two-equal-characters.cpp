f6d204f85231bdab1e3e5c5c1ba0b6d4dd68b398
dipayansaha2002 <121316056+dipayansaha2002@users.noreply.github.com>
2023-12-31 15:53:48 +0530

    Time: 3 ms (47.38%), Space: 6.7 MB (41.15%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.cpp
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int maxLengthBetweenEqualCharacters(string s) {
-        unordered_map<char, int> lastOccur;
+        unordered_map<char,int> lastOccur;
         int maxLen = -1;
-        for (int i = 0; i < s.length(); i++) {
+        for (int i=0; i<s.length(); i++) {
             char c = s[i];
             if (lastOccur.count(c)) {
-                maxLen = max(maxLen, i - lastOccur[c] - 1);
+                maxLen = max(maxLen, i-lastOccur[c]-1);
             } else {
                 lastOccur[c] = i;
             }
