99ea11feb04a61347e5d073e179db25a4aa44525
dipayansaha2002 <121316056+dipayansaha2002@users.noreply.github.com>
2024-01-03 23:09:04 +0530

    Time: 85 ms (76.99%), Space: 23.4 MB (82.87%) - LeetHub

---
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -0,0 +1,15 @@
+#pragma GCC optimize("O3", "unroll-loops")
+class Solution {
+public:
+    int numberOfBeams(vector<string>& bank) {
+        int ans=0, prev=0;
+        for(auto& row: bank){
+            int device=count(row.begin(), row.end(), '1');
+            if (device>0){
+                ans+=device*prev;
+                prev=device;
+            }
+        }
+        return ans;
+    }
+};
