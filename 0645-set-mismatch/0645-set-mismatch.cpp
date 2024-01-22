680e09ed7efd961e0a73a7b91cffcc273883b49b
dipayansaha2002 <121316056+dipayansaha2002@users.noreply.github.com>
2024-01-22 14:28:37 +0530

    Time: 72 ms (15.61%), Space: 35.4 MB (8.31%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        int x,y;
-        map<int,int>m;
-        int n=nums.size();
-        for(int i=0;i<n;i++)
-        {
-            m[nums[i]]++;
-
+        vector<int> res;
+        set<int> s;
+        for(int i=0;i<nums.size();++i){
+            if(s.count(nums[i]))
+                res.push_back(nums[i]);
+            else
+                s.insert(nums[i]);
         }
-        for(int i=0;i<n;i++)
-        {
-           if(m[i+1]==0)
-           x=i+1;
-           if(m[i+1]==2)
-           y=i+1;
-            
+        int idx = 1;
+        for(const auto& n: s){
+            if(n!=idx){
+                res.push_back(idx);
+                return res;
+            }
+            idx++;
         }
-        return {y,x};
+        res.push_back(idx);
+        return res;
     }
 };
