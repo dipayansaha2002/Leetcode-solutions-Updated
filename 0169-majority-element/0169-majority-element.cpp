bcaefc4ee7a021da8c665b47451a4b2278f223af
Dipayan Saha <121316056+dipayansaha2002@users.noreply.github.com>
2024-08-03 01:38:17 +0530

    Time: 21 ms (29.76%), Space: 26.8 MB (65.93%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,15 +1,18 @@
 class Solution {
- public:
-  int majorityElement(vector<int>& nums) {
-    int ans;
-    int count = 0;
-
-    for (const int num : nums) {
-      if (count == 0)
-        ans = num;
-      count += num == ans ? 1 : -1;
+public:
+    int majorityElement(vector<int>& nums) {
+        int majority = 0;
+        for (unsigned int i = 0, mask = 1; i < 32; i++, mask <<= 1) {
+            int bits = 0;
+            for (int num : nums) {
+                if (num & mask) {
+                    bits++;
+                }
+            }
+            if (bits > nums.size() / 2) {
+                majority |= mask;
+            }
+        }
+        return majority;
     }
-
-    return ans;
-  }
 };
