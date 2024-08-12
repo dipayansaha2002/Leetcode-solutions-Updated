b9bda46db84bd0419e9659cd8b4b56c3cc1b0dd0
Dipayan Saha <121316056+dipayansaha2002@users.noreply.github.com>
2024-08-13 01:41:56 +0530

    Time: 21 ms (5.53%), Space: 22.9 MB (25.05%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -11,18 +11,16 @@
  */
 class Solution {
 public:
-    void inOrderTraversal(TreeNode* root, vector<int> &v){
-        if(root == NULL)    
-            return;
-        
-        inOrderTraversal(root->left, v);
-        v.push_back(root->val);
-        inOrderTraversal(root->right, v);      
+    void inorder(TreeNode* root, vector<int>& V){
+        if(root==NULL) return;
+        inorder(root->left,V);
+        V.push_back(root->val);
+        inorder(root->right,V);
     }
-    
     int kthSmallest(TreeNode* root, int k) {
-        vector<int> v; 
-        inOrderTraversal(root, v);
-        return v[k-1];
+        vector<int> V;
+        inorder(root, V);
+        int Kth_smallest = V[k-1];
+        return Kth_smallest;
     }
 };
