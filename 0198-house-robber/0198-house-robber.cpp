46c4ba3afffb60871061a82c089e175c51c3139e
Dipayan Saha <121316056+dipayansaha2002@users.noreply.github.com>
2024-08-23 20:00:05 +0530

    Time: 0 ms (100.00%), Space: 9.3 MB (64.52%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
-    int findSumNonSubsequent(vector<int>& nums, int n, vector<int>& dp){
+    int findSumNonSubsequent(vector<int>& nums, int n){
         //Edge cases when size = 0/1
        if(n == 0) return 0;
        if(n == 1) return nums[0];
         
         
-       dp[0] = nums[0];
-       dp[1] = max(nums[0],nums[1]);
+       int prev = nums[0];
+       int prev2 = 0;
         
        for(int i=1; i<n; i++){
            
-           int pick = nums[i];
-           
-           if(i>1) 
-           {
-               pick += dp[i-2];
-               int notpick = 0 + dp[i-1];
+           //int pick = nums[i];
+        
+               int pick = nums[i] + prev2 ;
+               int notpick = 0 + prev;
                
-               dp[i] = max(pick, notpick);
-           }
+               int curr_i = max(pick, notpick);
+               prev2 = prev;
+               prev = curr_i;
+           
            
        }
         
-        return dp[n-1];
+        return prev;
     }
     
     
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, 0);
-        return findSumNonSubsequent(nums, n ,dp);
+       // vector<int> dp(n, 0);
+        return findSumNonSubsequent(nums, n);
     }
 };
