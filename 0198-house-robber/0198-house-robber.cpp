6810fc170087b0c781f9489c7cc0645e704e54ad
Dipayan Saha <121316056+dipayansaha2002@users.noreply.github.com>
2024-08-23 19:41:08 +0530

    Time: 4 ms (31.91%), Space: 9.6 MB (9.60%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,24 +1,35 @@
 class Solution {
 public:
-    int rob(std::vector<int>& nums) {
-        int len = nums.size();
-
-        if (len == 0) {
-            return 0;
-        } else if (len == 1) {
-            return nums[0];
-        } else if (len == 2) {
-            return std::max(nums[0], nums[1]);
-        }
-
-        std::vector<int> dp(len);
-        dp[0] = nums[0];
-        dp[1] = std::max(nums[0], nums[1]);
-
-        for (int i = 2; i < len; ++i) {
-            dp[i] = std::max(dp[i - 1], nums[i] + dp[i - 2]);
-        }
-
-        return dp[len - 1];
+    int findSumNonSubsequent(vector<int>& nums, int n, vector<int>& dp){
+        //Edge cases when size = 0/1
+       if(n == 0) return 0;
+       if(n == 1) return nums[0];
+        
+        
+       dp[0] = nums[0];
+       dp[1] = max(nums[0],nums[1]);
+        
+       for(int i=1; i<n; i++){
+           
+           int pick = nums[i];
+           
+           if(i>1) 
+           {
+               pick += dp[i-2];
+               int notpick = 0 + dp[i-1];
+               
+               dp[i] = max(pick, notpick);
+           }
+           
+       }
+        
+        return dp[n-1];
+    }
+    
+    
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, 0);
+        return findSumNonSubsequent(nums, n ,dp);
     }
 };
