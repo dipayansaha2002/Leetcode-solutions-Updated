7a7b5dbe80dcef926e8398a0ba2c1ebb2ad61832
Dipayan Saha <121316056+dipayansaha2002@users.noreply.github.com>
2024-09-27 19:09:17 +0530

    Time: 3 ms (97.30%), Space: 8.6 MB (93.66%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,11 +2,16 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.length() != t.length()) return false;
-        
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        
-        if(s == t) return true;
-        else return false;
+        int map[26] = {0};
+        for(char x : s){
+            map[x - 'a']++;
+        }
+        for(char x : t){
+            map[x - 'a']--;
+        }
+        for(int i : map){
+            if(i != 0) return false;
+        }
+        return true;
     }
 };
