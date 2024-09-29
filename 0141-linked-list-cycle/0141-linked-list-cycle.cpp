06bfe674c72567580f30a9fb54112e4dccad13ad
Dipayan Saha <121316056+dipayansaha2002@users.noreply.github.com>
2024-09-29 15:35:00 +0530

    Time: 6 ms (90.66%), Space: 10.9 MB (67.01%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -14,8 +14,7 @@ public:
         while(fast->next != NULL && fast->next->next != NULL){
             slow = slow->next;
             fast = fast->next->next;
-            
-            if(slow == fast) return true;
+            if(slow == fast) return true; //Cycle detected
         }
         return false;
     }
